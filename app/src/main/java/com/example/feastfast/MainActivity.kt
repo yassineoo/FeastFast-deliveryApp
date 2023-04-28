38634eb644cc17b518ae0fd83624ea3fcf60103b
Youcef Islam <jy_ghodbane@esi.dz>
2023-04-28 23:39:48 +0100

    fixed action bar error!!!!!

--- app/src/main/java/com/example/feastfast/MainActivity.kt
+++ app/src/main/java/com/example/feastfast/MainActivity.kt
@@ -29,7 +29,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_explore, R.id.navigation_orders, R.id.navigation_account
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
